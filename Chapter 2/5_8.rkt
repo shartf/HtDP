69d9cc86d1ef52c68faab3f00082ce1866a133ac
shartf <shartf@users.noreply.github.com>
2021-09-21 00:02:09 +0200

    Euclidian distance 3d

---
+++ Chapter 2/5_8.rkt
@@ -0,0 +1,19 @@
+#lang htdp/bsl
+(require 2htdp/universe)
+
+(define-struct r3 (x y z))
+; An R3 is a structure:
+; (make-r3 Number Number Number)
+
+(define ex1 (make-r3 1 2 13))
+(define ex2 (make-r3 -1 0 3))
+
+; R3 -> Number
+; determines the distance of p to the origin
+(check-expect (r3-distance-to-0 ex1) 13.19090595827292)
+(check-expect (r3-distance-to-0 ex2) 3.1622776601683795)
+
+(define (r3-distance-to-0 p)
+  (sqrt (+  (sqr (r3-x p))
+         (sqr (r3-y p))
+         (sqr (r3-z p)))))
