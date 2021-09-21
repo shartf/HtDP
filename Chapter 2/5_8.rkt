ee4939fd9192d5552473e34f5cc703cd3100259a
shartf <shartf@users.noreply.github.com>
2021-09-21 21:42:41 +0200

    Added a function to calculate seconds

--- Chapter 2/5_8.rkt
+++ Chapter 2/5_8.rkt
@@ -10,10 +10,25 @@
 
 ; R3 -> Number
 ; determines the distance of p to the origin
-(check-expect (r3-distance-to-0 ex1) 13.19090595827292)
-(check-expect (r3-distance-to-0 ex2) 3.1622776601683795)
+;(check-expect (r3-distance-to-0 ex1) 13.19090595827292)
+;(check-expect (r3-distance-to-0 ex2) 3.1622776601683795)
 
 (define (r3-distance-to-0 p)
   (sqrt (+  (sqr (r3-x p))
          (sqr (r3-y p))
          (sqr (r3-z p)))))
+
+;EXERCISE 80
+(define-struct time (h m s))
+; Time structure
+; (make-time Number Number Number)
+(define t1 (make-time 12 30 2))
+
+; Time -> Seconds
+; Consumes a time structure and returns a number of seconds passed since midnight
+(check-expect (time->seconds t1) 45002)
+
+(define (time->seconds t)
+  (+ (time-s t)
+     (* (time-m t) 60)
+     (* (time-h t) 3600)))
